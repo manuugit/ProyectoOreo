1fa6f1f3bf42cf1345ae45853f17f05a13cecf82
Candre <carlos.uribecupb.edu.co>
2022-10-17 19:34:44 -0500

    Modificaciones e implementaciones de la libreria

--- main.py
+++ main.py
@@ -5,59 +5,7 @@ import pyttsx3
 
 r = sr.Recognizer()
 engine = pyttsx3.init()
-
-Diccionario_sentimientos = {}
-
-# def EncenderLed():
-#     Board().led.state = Led.ON
-
-# def ApagarLed():
-#     try:
-#         Board().led.state = Led.OFF
-#     except(RuntimeError):
-#         pass
-
-# def EsperarClick():
-#     Board().button.wait_for_press()
-
-# def EsperarSoltar():
-#     Board().button.wait_for_release()
-
-def inicializarEngine():
-    voices = engine.getProperty('voices')
-    # for voice in voices:
-    #     print(voice)
-
-    #EN EL RASPI
-    # engine.setProperty('voice', voices[20].id)
-
-    #EN WINDOWS DEPENDE DE LA INSTALACION
-    engine.setProperty('voice', voices[0].id)
-    
-    engine.setProperty('rate', 140)
-
-def raspiHabla(text):
-    print(text)
-    engine.say(text)
-    engine.runAndWait()
-
-def reconocerVoz(seconds) -> String:
-    text = "Trying"
-    with sr.Microphone() as source:
-        # EncenderLed()
-        # EsperarClick()
-        # ApagarLed()
-        print("Iniciando Grabación de Audio...")
-        audio_data = r.record(source, duration=seconds)
-        print("Reconociendo...")
-        text = r.recognize_google(audio_data, language="es-ES")
-        # EsperarSoltar()
-    return text
-
-def crearDiccionario(): #Creación de diccionario
-    print("Prueba Diccionario")
-
-    Diccionario_sentimientos = {'Confundido':'Confusion',
+Diccionario_sentimientos = {'Confundido':'Confusion',
     'Vacio':'Hipotimia',
     'Solo':'Hipotimia',
     'Estresado':'Estres',
@@ -85,31 +33,79 @@ def crearDiccionario(): #Creación de diccionario
     'Orgulloso':'Orgulloso',
     'Decaido':'Decaido'}
 
+# def EncenderLed():
+#     Board().led.state = Led.ON
+
+# def ApagarLed():
+#     try:
+#         Board().led.state = Led.OFF
+#     except(RuntimeError):
+#         pass
+
+# def EsperarClick():
+#     Board().button.wait_for_press()
+
+# def EsperarSoltar():
+#     Board().button.wait_for_release()
+
+def inicializarEngine():
+    voices = engine.getProperty('voices')
+    # for voice in voices:
+    #     print(voice)
+
+    #EN EL RASPI
+    # engine.setProperty('voice', voices[20].id)
+
+    #EN WINDOWS DEPENDE DE LA INSTALACION
+    engine.setProperty('voice', voices[2].id)
+    
+    engine.setProperty('rate', 140)
+
+def raspiHabla(text):
+    print(text)
+    engine.say(text)
+    engine.runAndWait()
+
+def reconocerVoz(seconds) -> String:
+    text = "Trying"
+    with sr.Microphone() as source:
+        # EncenderLed()
+        # EsperarClick()
+        # ApagarLed()
+        print("Iniciando Grabación de Audio...")
+        audio_data = r.record(source, duration=seconds)
+        print("Reconociendo...")
+        text = r.recognize_google(audio_data, language="es-ES")
+        # EsperarSoltar()
+    return text
+
+
 
 def getKeyWords(text,array): #Texto de usuario, Array de posibles respuestas
     retArray = []
     text = text.upper()
     print(text)
     for palabra in array:
-        if text.__contains__(palabra):
+        palabre = palabra[:len(palabra)-1]+'a'
+        # palabre[len(palabre)-1] = 'a'
+        if text.__contains__(palabra.upper()):
             retArray.append(palabra)
+        if text.__contains__(palabre.upper()):
+            retArray.append(palabre)
     if retArray == []:
         return "No hay elementos"        
     return retArray
 
 
 def main():
-    crearDiccionario()
     inicializarEngine()
     while True:
         raspiHabla("Bienvenido Señor Usuario")
         raspiHabla("Por favor seleccione una de las siguientes opciones")
         raspiHabla("Diga 'Reportar estado de ánimo' para recomendarte algo según tu estado de ánimo")
-        print(getKeyWords(reconocerVoz(4),['JEAN','LINDO']))
+        print(getKeyWords(reconocerVoz(7),list(Diccionario_sentimientos.keys())))
 
 
-
-    
 
 if __name__ == "__main__":
     main()
