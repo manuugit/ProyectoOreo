9024b4dff92624b88642d479f4778d2a3975b50b
Candre <carlos.uribecupb.edu.co>
2022-10-26 15:30:45 -0500

    Prediccion Implementada

--- demo.py
+++ demo.py
@@ -7,6 +7,7 @@ import numpy as np  # matrices y vectores
 #import matplotlib.pyplot as plt #gráfica
 import sqlite3 #La BD
 # from aiy.board import Board, Led
+import pickle # Para abrir el modelo predictivo
 
 r = sr.Recognizer()
 engine = pyttsx3.init()
@@ -67,6 +68,11 @@ Diccionario_gustos = {1:'Actividad Física',
 
 conn = sqlite3.connect("nombre_gustos.sqlite")
 cur = conn.cursor()
+
+#Modelo Predictivo y afines
+archivoModelo = 'modeloPredictivo.pkl'
+modeloKNN, variablesModelo = pickle.load(open(archivoModelo, 'rb'))
+
 
 def insertarBD(sentencia):
     conn2 = sqlite3.connect("nombre_gustos.sqlite")
@@ -163,8 +169,25 @@ def getKeyWords(text,array): #Texto de usuario, Array de posibles respuestas
         return "No hay elementos"       
     return retArray
 
-# def Recomendar(nombre):
-#     if
+def prediccionCluster(gustos, sentimientos):
+    #Ajuste del dataframe para pasar por el modelo
+    valores = pd.DataFrame()
+    i = 0
+    for gusto in gustos:
+        valores.insert(i, gusto, [1])
+        i = i+1
+    for sentimiento in sentimientos:
+        valores.insert(i, sentimiento, [1])
+        i = i+1
+
+    #Reindexamos con las variables del modelo / Se adicionan las columnas faltantes
+    valores=valores.reindex(columns=variablesModelo,fill_value=0)
+
+
+    #Realizamos la prediccion
+    prediccion = modeloKNN.predict(valores)
+    return prediccion
+    
     
 def main():
     inicializarEngine()
@@ -175,9 +198,6 @@ def main():
         # ApagarLed()
         raspiHabla("¡Hola! Mi nombre es Oreo. Soy un asistente de voz con un solo propósito")
         raspiHabla("¡Quiero ayudarte a sentirte mejor!")
-        # EncenderLed()
-        # EsperarClick()
-        # ApagarLed()
         raspiHabla("Por favor cuando el botón se ilumine, mantenlo presionado y dime tu nombre")
         # EncenderLed()
         # EsperarClick()
@@ -190,7 +210,9 @@ def main():
 
         #Obtener nombre si existe en la BD
         valNombre = cur.execute("select nombre from usuarios where nombre=?",(nombre,)).fetchone()
+        
         # Si el nombre no existe se trata de un usuario nuevo
+        gustos = []
         if(valNombre==None):
             insertarBD("insert into usuarios(nombre) values('{0}')".format(nombre))
             idUsuario = cur.execute("select * from usuarios where nombre=?",(nombre,)).fetchone()[0]
@@ -206,19 +228,33 @@ def main():
                     break
                 elif(int(opcion) >0 and int(opcion)<27):
                     insertarBD("insert into gustos(gusto,idUsuario) values('{0}',{1})".format(Diccionario_gustos.get(int(opcion)), idUsuario))
+                    gustos.append(Diccionario_gustos.get(int(opcion)))
                                 
         else:
             gustos = cur.execute("SELECT gusto FROM gustos INNER JOIN usuarios ON gustos.idUsuario = usuarios.idUsuario WHERE nombre = ?",(nombre,)).fetchall()
             gustos = list(map(lambda x: x[0],gustos))
             raspiHabla("Hola "+nombre+", lamento que estes así")
             raspiHabla("Noto que te gusta: {0}".format(' ,'.join(map(str,gustos))))
-            raspiHabla("Según mi algoritmo. ¿Que te parece sí pintas algo para sentirte mejor?")
-            raspiHabla("No tiene sentido que te recomiende algo que te gusta, supongo que ya lo intentaste.")
-            raspiHabla("Sé que quizás no te consideres bueno haciéndolo, pero dale un intento.")
-            print(gustos)
+            # raspiHabla("Según mi algoritmo. ¿Que te parece sí pintas algo para sentirte mejor?")
+            # raspiHabla("No tiene sentido que te recomiende algo que te gusta, supongo que ya lo intentaste.")
+            # raspiHabla("Sé que quizás no te consideres bueno haciéndolo, pero dale un intento.")
+            # print(gustos)
+        
+        raspiHabla("Cuando el boton se ilumine presionalo y dime cual es tu estado de animo")
+        # EncenderLed()
+        # EsperarClick()
+        # ApagarLed()
+        sentimientos = getKeyWords(reconocerVoz(7),list(Diccionario_sentimientos.keys()))
+        print(gustos)
+        print(sentimientos)
 
-        # raspiHabla("Diga 'Reportar estado de ánimo' para recomendarte algo según tu estado de ánimo")
-        # print(getKeyWords(reconocerVoz(7),list(Diccionario_sentimientos.keys())))
+        for i in range(0, len(gustos)):
+            gustos[i] = gustos[i].replace(' ', '_')
+        for i in range(0, len(sentimientos)):
+            sentimientos[i] = "Diario."+sentimientos[i]
+
+        clusterAsignado = prediccionCluster(gustos, sentimientos)
+        break
         
 
 
