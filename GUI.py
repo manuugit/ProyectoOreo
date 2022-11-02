8de483d279f40d5b99e050edafb8f60c3d20640b
Miguel Gallego <miguel.gallego@upb.edu.co>
2022-11-02 16:48:29 -0500

    Implementaciones backend

--- GUI.py
+++ GUI.py
@@ -2,6 +2,34 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+Diccionario_sentimientos = {'Confundido':'Confusion',
+    'Vacío':'Hipotimia',
+    'Solo':'Hipotimia',
+    'Estresado':'Estres',
+    'Angustiado':'Hipotimia',
+    'Eufórico':'Euforia',
+    'Ansioso':'Ansiedad',
+    'Tranquilo':'Tranquilidad',
+    'Emocionado':'Energico',
+    'Asustado':'Ansiedad',
+    'Decepcionado':'Hipotimia',
+    'Desesperado':'Ansiedad',
+    'Enérgico':'Energico',
+    'Cansado':'Cansancio',
+    'Agotado':'Cansancio',
+    'Animado':'Euforia',
+    'Motivado':'Euforia',
+    'Desmotivado':'Hipotimia',
+    'Aburrido':'Hipotimia',
+    'Melancólico':'Hipotimia',
+    'Humillado':'Hipotimia',
+    'Frustrado':'Depresion',
+    'Enojado':'Enojo',
+    'Abrumado':'Hipotimia',
+    'Envidioso':'Envidioso',
+    'Orgulloso':'Orgulloso',
+    'Decaído':'Decaido'}
+
 Diccionario_gustos = {1:'Actividad Física',
     2:'Gastronomía',
     3:'Arte',
@@ -42,13 +70,22 @@ def index():
 def gustos():
     if request.method == 'GET':
         pass
+    if request.method == 'POST':
+        nombre = request.form.get('nombre')
+        return render_template('gustos.html', data = Diccionario_gustos, nombre = nombre)
+
     return render_template('gustos.html', data = Diccionario_gustos)
 
 @app.route('/recomendacion', methods=['GET','POST'])
 def recomendacion():
     if request.method == 'GET':
         pass
-    return render_template('recomendacion.html', data = None)
+    if request.method == 'POST':
+        sentimientos=request.form.getlist('sentimiento')
+        sentimiento = max(set(sentimientos), key=sentimientos.count)
+        return render_template('recomendacion.html', data = Diccionario_sentimientos, sentimiento=sentimiento)
+    
+    return render_template('recomendacion.html', data = Diccionario_sentimientos)
 
 @app.route('/signup', methods=['GET','POST'])
 def signup():
